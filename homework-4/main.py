a91c2d52842509212b15782884f0e327b6623c40
yana <yanaposelenceva@gmail.com>
2023-06-28 14:47:08 +0300

    fixes

--- homework-4/main.py
+++ homework-4/main.py
@@ -4,7 +4,6 @@ if __name__ == '__main__':
     # Создаем два экземпляра класса
     video1 = Video('AWX4JnAnjBE')  # 'AWX4JnAnjBE' - это id видео из ютуб
     video2 = PLVideo('4fObz_qw9u4', 'PLv_zOGKKxVph_8g2Mqc3LMhj0M_BfasbC')
-    video1.add_attr()
-    video2.add_attr()
+
     assert str(video1) == 'GIL в Python: зачем он нужен и как с этим жить'
     assert str(video2) == 'MoscowPython Meetup 78 - вступление'
--- src/video.py
+++ src/video.py
@@ -2,20 +2,9 @@ import os
 from googleapiclient.discovery import build
 from src.channel import Channel
 
-
 class Video:
     def __init__(self, video_id):
         self.video_id = video_id
-        self.title = None
-        self.url = None
-        self.view_count = None
-        self.like_count = None
-        self.comment_count = None
-
-    def __str__(self):
-        return f'{self.title}'
-
-    def add_attr(self):
         api_key: str = os.getenv('YT_API_KEY')
         youtube = build('youtube', 'v3', developerKey=api_key)
         video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
@@ -29,8 +18,13 @@ class Video:
         self.comment_count: int = video_response['items'][0]['statistics']['commentCount']
 
 
+    def __str__(self):
+        return f'{self.title}'
+
+
 class PLVideo(Video):
     def __init__(self, video_id, pl):
         super().__init__(video_id)
         self.pl = pl
+
 
